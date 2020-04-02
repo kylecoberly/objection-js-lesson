e007d91ba84a370b405e66ccf117aaae9f87b7eb
Kyle Coberly <kyle.coberly@gmail.com>
2020-04-02 12:08:23 -0600

    Removing solution

--- index.js
+++ index.js
@@ -8,64 +8,8 @@ const databaseConfig = require("./knexfile")[process.env.NODE_ENV || "developmen
 const knex = require("knex")
 const database = knex(databaseConfig)
 
-const { Model } = require("objection")
-Model.knex(database)
-
-class Owner extends Model {
-  static get tableName() {
-    return "owner"
-  }
-
-  static get relationMappings(){
-    return {
-      dogs: {
-        relation: Model.ManyToManyRelation,
-        modelClass: Dog,
-        join: {
-          from: "owner.id",
-          through: {
-            from: "dog_owner.owner_id",
-            to: "dog_owner.dog_id",
-          },
-          to: "dog.id",
-        }
-      }
-    }
-  }
-}
-
-class Dog extends Model {
-  static get tableName() {
-    return "dog"
-  }
-
-  static get relationMappings(){
-    return {
-      owners: {
-        relation: Model.ManyToManyRelation,
-        modelClass: Owner,
-        join: {
-          from: "dog.id",
-          through: {
-            from: "dog_owner.dog_id",
-            to: "dog_owner.owner_id",
-          },
-          to: "owner.id",
-        }
-      }
-    }
-  }
-}
-
-class DogOwner extends Model {
-  static get tableName() {
-    return "dog_owner"
-  }
-}
-
 app.get("/", async (request, response) => {
-  const dogs = await Dog.query().withGraphFetched("owners")
-  response.json({ dogs })
+  response.json({ message: "Hi!" })
 })
 
 app.listen(process.env.PORT || 4000)
